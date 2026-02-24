192211e38aa41f3969cee9202f11e5d2060e0968
aitorablab <aitorablab@gmail.com>
2026-02-24 08:38:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/printiaproject/",
   server: {
     host: "::",
     port: 8080,
